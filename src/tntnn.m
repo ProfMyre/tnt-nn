e2e10f29f235c299063b71ea009b94a729ff7e02
MyreResearchGroup <myre@stthomas.edu>
2018-08-17 13:18:35 -0500

    Add files via upload

--- src/tntnn.m
+++ src/tntnn.m
@@ -500,10 +500,8 @@ function [ x, k ] = pcgnr ( A, b, R )
     x = zeros(n,1);
     r = b;
     r_hat = A' * r; % matrix_x_vector, O(mn)
-    y = cs_ltsolve(R', r_hat);
-    z = cs_ltsolve(R, y);
-    %y = R' \ r_hat; % back_substitution, O(n^2)
-    %z = R \ y; % back_substitution, O(n^2)
+    y = R' \ r_hat; % back_substitution, O(n^2)
+    z = R \ y; % back_substitution, O(n^2)
     p = z;
     gamma = dot(z,r_hat);
     prev_rr = -1;
@@ -540,5 +538,57 @@ function [ x, k ] = pcgnr ( A, b, R )
             return;
         end
     end
+end
+
+% ====================================================================
+% Compute a new Cholesky factor after deletion of some variables.
+% ====================================================================
+% Author: Erich Frahm, frahm@physics.umn.edu
+% ====================================================================
+function R = cholesky_delete(R,BB,deletion_set)
+
+[ m n ] = size(R);
+[ r c ] = size(deletion_set);
+num_deletions = max(r,c);
+
+speed_fudge_factor = 0.001;
+if (num_deletions > (speed_fudge_factor * n))
+
+    % =============================================================
+    % Full Cholesky decomposition of BB (on GPUs).
+    % =============================================================
+    [R,p] = chol(BB); % O(n^3/3)
+    if (p > 0)
+        % This should never happen because we have already added
+        % a sufficiently large "epsilon" to AA to do the
+        % nonnegativity tests required to create the deleted_set.
+        dummy_var = fail_here1; % fail_here1 is not defined.
+    end
+    
+else
+
+    for i=1:num_deletions
+        j = deletion_set(i);
+        
+        % =============================================================
+        % This function is just a stripped version of Matlab's qrdelete.
+        % Stolen from:
+        % http://pmtksupport.googlecode.com/svn/trunk/lars/larsen.m
+        % =============================================================
+        R(:,j) = []; % remove column j
+        n = size(R,2);
+        for k = j:n
+            p = k:k+1;
+            [G,R(p,k)] = planerot(R(p,k)); % remove extra element in col
+            if k < n
+                R(p,k+1:n) = G*R(p,k+1:n); % adjust rest of row
+            end
+        end
+        R(end,:) = []; % remove zero'ed out row
+        
+    end
+end
+
+return;
 end
 
